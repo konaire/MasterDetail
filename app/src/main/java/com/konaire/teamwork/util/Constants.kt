9ded727487493ce1873a8ed26ec932588d24e753
Evgeny Eliseyev <eugvelis@gmail.com>
2018-07-16 09:52:50 +0300

    removed critical data to local properties

--- app/src/main/java/com/konaire/teamwork/util/Constants.kt
+++ app/src/main/java/com/konaire/teamwork/util/Constants.kt
@@ -1,11 +1,13 @@
 package com.konaire.teamwork.util
 
+import com.konaire.teamwork.BuildConfig
+
 /**
  * Created by Evgeny Eliseyev on 23/04/2018.
  */
 class Constants {
     companion object {
-        const val BASE_URL = "https://yat.teamwork.com"
-        const val API_TOKEN = "twp_k9ejP88LcuojHjmFkUFuYIUNYalg"
+        const val BASE_URL = BuildConfig.BASE_URL
+        const val API_TOKEN = BuildConfig.API_TOKEN
     }
 }
